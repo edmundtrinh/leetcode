a704d71f7f95c43a1f79f2b3e88eb3736f2aa80a
Bruce Wayne <edmund.trinh@gmail.com>
2020-02-27 00:23:35 -0800

    First attempt #1342

---
+++ algorithms/java/src/1342.NumberOfStepsToReduceANumberToZero.java
@@ -0,0 +1,56 @@
+/*
+    Author:     Edmund Trinh, edmund.trinh@gmail.com
+    Date:       Feb 23, 2020
+    Update:     Feb 23, 2020
+    Problem:    1342. Number of Steps to Reduce a Number to Zero
+    Difficulty: Easy
+    Source:     https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
+    
+    Notes:
+        Given a non-negative integer num, return the number of steps to reduce it to zero.
+        If the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.
+        
+    Examples:
+    1.
+    Input: num = 14
+    Output: 6
+    Explanation: 
+    Step 1) 14 is even; divide by 2 and obtain 7. 
+    Step 2) 7 is odd; subtract 1 and obtain 6.
+    Step 3) 6 is even; divide by 2 and obtain 3. 
+    Step 4) 3 is odd; subtract 1 and obtain 2. 
+    Step 5) 2 is even; divide by 2 and obtain 1. 
+    Step 6) 1 is odd; subtract 1 and obtain 0.
+    
+    2.
+    Input: num = 8
+    Output: 4
+    Explanation: 
+    Step 1) 8 is even; divide by 2 and obtain 4. 
+    Step 2) 4 is even; divide by 2 and obtain 2. 
+    Step 3) 2 is even; divide by 2 and obtain 1. 
+    Step 4) 1 is odd; subtract 1 and obtain 0.
+    
+    3.
+    Input: num = 123
+    Output: 12
+    
+    Constraints:
+    0 <= n <= 10^6
+*/
+
+class Solution {
+    public int numberOfSteps (int num) {
+        // Assuming input is always within constraints: 0 <= num <= 10^6
+        int count = 0;
+        while (num != 0) {
+            if (num % 2 == 0) {
+                num /= 2;
+            } else {
+                num--;
+            }
+            count++;
+        }
+        return count;
+    }
+}
